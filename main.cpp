c58c708daff28d57f0f9f84e730ebfc3b462a4c1
Fred Akers <knivey@botops.net>
2016-04-09 08:18:11 -0400

    Update to add /clip option
    Signed-off-by: Fred Akers <knivey@botops.net>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <windows.h>
 #include <malloc.h>
 #include <cstring>
+#include <string>
 
 using namespace std;
+
+string output;
 
 int ewcb(HWND hWin, LPARAM lParam)
 {
@@ -14,14 +17,37 @@ int ewcb(HWND hWin, LPARAM lParam)
 		if((cutoff = strstr(title, " [foobar2000 v")) != NULL) {
 			*cutoff = 0;
 			cout << title << endl;
+			output = title;
 		}
 	}
 	return TRUE;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 	EnumWindows(ewcb, 0);
+	if(argc > 1 && !strcmp("/clip", argv[1])) {
+		if(!OpenClipboard(NULL)) {
+			cout << "Cannot open the Clipboard" << endl;
+			return -1;
+		}
+		if(!EmptyClipboard()) {
+			cout << "Cannot empty the Clipboard" << endl;
+			return -1;
+		}
+		HGLOBAL hGlob = GlobalAlloc(GMEM_FIXED, 512);
+		strcpy_s((char*)hGlob, 512, output.c_str());
+
+		if(SetClipboardData(CF_TEXT, hGlob) == NULL)	{
+			cout << "Unable to set Clipboard data, error: " << GetLastError() << endl;
+			CloseClipboard();
+			GlobalFree(hGlob);
+			return -1;
+		}
+		//ClipBoard is now responsible for hGlob
+		CloseClipboard();
+		cout << "Copied to clipboard" << endl;
+	}
 	return 0;
 }
 
